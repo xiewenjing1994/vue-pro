6d9beae8cb08b2680bc07dbf1bccdb9cdd2f58bb
xiewenjing <xiewenjing1994@163.com>
2019-02-15 15:57:31 +0800

    登录功能开发完毕

--- src/main.js
+++ src/main.js
@@ -4,12 +4,28 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入全局css
+import './assets/css/global.css'
+// 引入element-ui
+import ElementUI from 'element-ui'
+
+// 引入登录页icon图标
+import './assets/fonts/iconfont.css'
+
+// 引入axios
+
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
