226d013366a993c27ad3ec45f4118e4044f77330
xiewenjing <xiewenjing1994@163.com>
2019-02-15 20:03:55 +0800

    后台首页面开发完毕

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,117 @@
 <template>
-    <div>
-      <h2>后台首页</h2>
-    </div>
+  <el-container>
+    <el-header>
+      <div>
+        <img src="../assets/img/heima.png"/>
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="cancelbtn">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside :width="menushow?'60px':'200px'">
+        <div
+        style="color: white;height: 25px;line-height: 25px;font-size: 12px;background-color:#4a5064;letter-spacing: 0.1em;cursor: pointer;user-select:none;"
+        @click="menushow = !menushow"
+        >|||</div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          :style="menushow?'width=60px;':'width=200px;'"
+          :collapse="menushow"
+          :collapse-transition="false"
+          :unique-opened="true"
+        >
+          <el-submenu :index="item.id + ''" v-for="(item,k) in menuList" :key="item.id">
+            <template slot="title">
+              <i :class="'iconfont icon-'+menuicon[k]"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <el-menu-item
+              index="item.id + '-' + item2.id"
+              v-for="item2 in item.children"
+              :key="item2.id"
+            >
+              <i class="el-icon-menu"></i>
+              {{ item2.authName }}
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
     export default {
-
+      created() {
+        this.getMenuListdata();
+      },
+      data() {
+        return {
+          // 设置菜单展开、收起
+          menushow: false,
+          menuList: [],
+          menuicon: ['users', 'tijikongjian', 'shangpin', 'danju', 'baobiao']
+        }
+      },
+      methods: {
+        // 退出登录
+        cancelbtn() {
+          this.$confirm('确定要退出吗','提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+          })
+            .then(() => {
+              window.sessionStorage.removeItem('token');
+              this.$router.push('/login');
+            })
+            .catch(() =>{ })
+        },
+        // 获取左侧导航数据
+        async getMenuListdata() {
+          const { data: res } = await this.$http.get('menus');
+          if (res.meta.status !== 200) {
+            return this.$message.error(res.meta.msg);
+          }
+          this.menuList = res.data;
+          // console.log(this.menuList)
+        }
+      }
     }
 </script>
 
 <style lang="less" scoped>
-
+    .el-container {
+      height: 100%;
+      .el-header {
+        padding: 0 20px 0 0;
+        background-color: #373d41;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        user-select:none;
+        div {
+          font-size: 17px;
+          color: #fff;
+          display: flex;
+          align-items: center;
+          img {
+            width: 50px;
+            height: 50px;
+            margin-right: 10px;
+          }
+        }
+      }
+      .el-aside {
+        background-color: #333744;
+      }
+      .el-main {
+        background-color: #eaedf1;
+      }
+    }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,31 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
+import Welcome from '@/components/Welcome'
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
+var router = new Router({
   routes: [
-    { path: '/', redirect: '/login' },
+    // { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    { path: '/home', component: Home, redirect: '/welcome', children: [
+        { path: '/welcome', component: Welcome }
+      ]}
   ]
-})
+});
+
+// 路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    return next();
+  }
+  // 如果访问的路径不是则查看是否携带有token值，若没有则返回登录页
+  var token = window.sessionStorage.getItem('token');
+  if (!token) {
+    return next('/login');
+  }
+  next();
+});
+
+export default router;
